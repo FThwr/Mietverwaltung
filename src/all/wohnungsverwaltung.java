b725c300e4d2d5aa89d1b6215818174c3aa1d791
FThwr <s_tsakonas@stud.hwr-berlin.de>
2017-07-13 02:17:45 +0200

    Update wohnungsverwaltung.java
    Hab nur die data experts Schrift rausgenommen

--- src/all/wohnungsverwaltung.java
+++ src/all/wohnungsverwaltung.java
@@ -1,18 +1,3 @@
-/*  
- * ----------------------------------------------------------------------------
- *     (c) by data experts gmbh
- *            Postfach 1130
- *            Woldegker Str. 12
- *            17001 Neubrandenburg
- * ----------------------------------------------------------------------------
- *     Dieses Dokument und die hierin enthaltenen Informationen unterliegen
- *     dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
- *     Herausgebers weder als ganzes noch in Teilen dupliziert, reproduziert
- *     oder manipuliert werden.
- * ----------------------------------------------------------------------------
- *     
- * ----------------------------------------------------------------------------
- */
 package all;
 
 public class wohnungsverwaltung {
