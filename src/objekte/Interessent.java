efc9f74e6a285009a9075d561d9104be94892b5d
AG1601 <alicia.guhl@arcor.de>
2017-08-05 16:04:10 +0200

    Kunde ist jetzt eine abstrakte Klasse

--- src/objekte/Interessent.java
+++ src/objekte/Interessent.java
@@ -1,9 +1,36 @@
 package objekte;
 
+import hilfsklassen.Adresse;
+import hilfsklassen.Datum;
+
 public class Interessent extends Kunde {
 
-	public Interessent() {
-		super();
+	private String rolle;
+	
+	public Interessent(int kundenID, String name, String vorname, Datum geburtsdatum, Adresse adresse, String email,
+			String telefonnummer) {
+		super(kundenID, name, vorname, geburtsdatum, adresse, email, telefonnummer);
+		this.setRolle("Interessent");
 	}
+
+	@Override
+	public String toString() {
+		return "Interessent: \nKunden-ID:" + kundenID + "\nName: " + name + "\nVorname: " + vorname + "\nGeburtsdatum: "
+				+ geburtsdatum.toString() + "\nAdresse: " + adresse.toString() + "\nE-Mail: " + email + "\nTelefonnummer: " + telefonnummer + "\nRolle: " + rolle;
+	}
+	
+	@Override
+	public void display() {
+		System.out.println(this.toString());
+	}
+	
+	public String getRolle() {
+		return rolle;
+	}
+
+	public void setRolle(String rolle) {
+		this.rolle = rolle;
+	}
+
 
 }
--- src/objekte/Mieter.java
+++ src/objekte/Mieter.java
@@ -1,7 +1,34 @@
 package objekte;
 
+import hilfsklassen.Adresse;
+import hilfsklassen.Datum;
+
 public class Mieter extends Kunde {
-	public Mieter() {
-		super();
+
+	private String rolle;
+	
+	public Mieter(int kundenID, String name, String vorname, Datum geburtsdatum, Adresse adresse, String email,
+			String telefonnummer) {
+		super(kundenID, name, vorname, geburtsdatum, adresse, email, telefonnummer);
+		this.setRolle("Mieter");
+	}
+	
+	@Override
+	public String toString() {
+		return "Mieter: \nKunden-ID:" + kundenID + "\nName: " + name + "\nVorname: " + vorname + "\nGeburtsdatum: "
+				+ geburtsdatum.toString() + "\nAdresse: " + adresse.toString() + "\nE-Mail: " + email + "\nTelefonnummer: " + telefonnummer + "\nRolle: " + rolle;
+	}
+	
+	@Override
+	public void display() {
+		System.out.println(this.toString());
+	}
+
+	public String getRolle() {
+		return rolle;
+	}
+
+	public void setRolle(String rolle) {
+		this.rolle = rolle;
 	}
 }
