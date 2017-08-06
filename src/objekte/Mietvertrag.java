cb1f1c53fec4d23d4fcef75e9f99a5e644b2604d
AG1601 <alicia.guhl@arcor.de>
2017-08-06 15:21:07 +0200

    Konstruktor bearbeitet

--- src/objekte/Mietvertrag.java
+++ src/objekte/Mietvertrag.java
@@ -1,9 +1,21 @@
 package objekte;
 
+import hilfsklassen.Datum;
+
 public class Mietvertrag {
 	
-	public Mietvertrag() {
-		
+	private int mietvertragsID;
+	private int wohnungsID;
+	private int kundenID;
+	private Datum mietBeginn;
+	private Datum mietEnde;
+	
+	public Mietvertrag(int mietvertragID, int wohnungsID, int kundenID, int mitarbeiterID, Datum mietBeginn, Datum mietEnde) {
+		this.mietvertragsID = mietvertragID;
+		this.wohnungsID = wohnungsID;
+		this.kundenID = kundenID;
+		this.mietBeginn = mietBeginn;
+		this.mietEnde = mietEnde;
 	}
 
 }
