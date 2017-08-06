fb935495aa03db27b927679b32844ff2b36d3614
AG1601 <alicia.guhl@arcor.de>
2017-08-06 16:19:19 +0200

    Mitarbeiter aktualisiert

--- src/objekte/Mitarbeiter.java
+++ src/objekte/Mitarbeiter.java
@@ -2,24 +2,27 @@ package objekte;
 
 public class Mitarbeiter {
 	
-	private String passwort;
 	private String name;
+	private String vorname;
+	private String benutzername;
+	private String passwort;
 	
-	public Mitarbeiter (String passwort, String name) {
-		this.passwort = passwort;
+	
+	public Mitarbeiter (String name, String vorname, String passwort) {
 		this.name = name;
+		this.passwort = passwort;
+		this.benutzername = vorname.trim().substring(0, 1) + "." + name;
 	}
 	
-	public Mitarbeiter (String name) {
+	public Mitarbeiter (String name, String vorname) {
 		this.name = name;
+		this.vorname = vorname;
 	}
 
-	public String getPasswort() {
-		return passwort;
-	}
-
-	public void setPasswort(String passwort) {
-		this.passwort = passwort;
+	@Override
+	public String toString() {
+		return "Mitarbeiter: Name: " + name + "\nVorname: " + vorname + "\nBenutzername: " + benutzername + "\nPasswort: "
+				+ passwort;
 	}
 
 	public String getName() {
@@ -30,5 +33,29 @@ public class Mitarbeiter {
 		this.name = name;
 	}
 	
+	public String getVorname() {
+		return vorname;
+	}
 
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getBenutzername() {
+		return benutzername;
+	}
+
+	public void setBenutzername(String benutzername) {
+		this.benutzername = benutzername;
+	}
+
+	public String getPasswort() {
+		return passwort;
+	}
+
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
+
+	
 }
