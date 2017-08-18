5feca166b8ab234bb3cb6038167605163ec40c40
Felicia Tsakonas <felicia.tsakonas@data-experts.de>
2017-08-18 07:21:22 +0200

    Datum Konstruktor

--- src/mietverwaltung/Datum.java
+++ src/mietverwaltung/Datum.java
@@ -9,7 +9,11 @@ public class Datum implements Comparable<Datum> {
     private int monat;
     private int jahr;
 
-    GregorianCalendar cal = new GregorianCalendar();
+    private static final GregorianCalendar cal = new GregorianCalendar();
+
+    public Datum() {
+        this(Datum.cal.get(Calendar.DATE), 1 + Datum.cal.get(Calendar.MONTH), Datum.cal.get(Calendar.YEAR));
+    }
 
     public Datum(final int tag, final int monat, final int jahr) {
         this.tag = tag;
@@ -41,17 +45,6 @@ public class Datum implements Comparable<Datum> {
         this.jahr = jahr;
     }
 
-    public Datum() {
-        this.tag = this.cal.get(Calendar.DATE);
-        this.monat = 1 + this.cal.get(Calendar.MONTH);
-        this.jahr = this.cal.get(Calendar.YEAR);
-
-    }
-
-    public void print() {
-        System.out.println(this.tag + "." + this.monat + "." + this.jahr);
-    }
-
     @Override
     public String toString() {
         return (this.tag + "." + this.monat + "." + this.jahr);
@@ -63,20 +56,20 @@ public class Datum implements Comparable<Datum> {
             return 1;
         } else if (this.jahr < arg0.jahr) {
             return -1;
-        } else {
-            if (this.monat > arg0.monat) {
-                return 1;
-            } else if (this.monat < arg0.monat) {
-                return -1;
-            } else {
-                if (this.tag > arg0.tag) {
-                    return 1;
-                } else if (this.tag < arg0.tag) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
         }
+
+        if (this.monat > arg0.monat) {
+            return 1;
+        } else if (this.monat < arg0.monat) {
+            return -1;
+        }
+
+        if (this.tag > arg0.tag) {
+            return 1;
+        } else if (this.tag < arg0.tag) {
+            return -1;
+        }
+
+        return 0;
     }
 }
