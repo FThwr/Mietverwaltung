84ecc87657677eee3d0e1fe841db6f3e965ed5a1
AG1601 <alicia.guhl@arcor.de>
2017-08-27 12:35:59 +0200

    Mitarbeiter seperat erstellt

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,5 @@
 package test;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -38,12 +37,19 @@ public class Test {
     private static MenuManager generieren() throws FileNotFoundException, ClassNotFoundException, IOException {
         MenuManager m = new MenuManager();
         // Anmeldekonten der Mitarbeiter
-        m.add(new Mitarbeiter(1, "Faustmann", "Gert", "Qualle"));
-        m.add(new Mitarbeiter(2, "Tsakonas", "Felicia", "Boing"));
-        m.add(new Mitarbeiter(3, "Bebenroth", "Mandy", "Boom"));
-        m.add(new Mitarbeiter(4, "Ruhkieck", "Sophia", "Zisch"));
-        m.add(new Mitarbeiter(5, "Guhl", "Alicia", "Rumps"));
-        m.add(new Mitarbeiter(6, "Menz", "Max", "Wusch"));
+        
+        Mitarbeiter mFaustmann = new Mitarbeiter(1, "Faustmann", "Gert", "Qualle");
+        Mitarbeiter mTsakonas = new Mitarbeiter(2, "Tsakonas", "Felicia", "Boing");
+        Mitarbeiter mBebenroth = new Mitarbeiter(3, "Bebenroth", "Mandy", "Boom");
+        Mitarbeiter mRuhkieck = new Mitarbeiter(4, "Ruhkieck", "Sophia", "Zisch");
+        Mitarbeiter mGuhl = new Mitarbeiter(5, "Guhl", "Alicia", "Rumps");
+        Mitarbeiter mMenz = new Mitarbeiter(6, "Menz", "Max", "Wusch");
+        m.add(mFaustmann);
+        m.add(mTsakonas);
+        m.add(mBebenroth);
+        m.add(mRuhkieck);
+        m.add(mGuhl);
+        m.add(mMenz);
 
         m.add(new Interessent(4021, "Mustermann", "Max", new Datum(3, 9, 2012), new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "mustermann@web.de", "03783476367", "Interessent"));
         m.add(new Interessent(4022, "Mustermann", "Max", new Datum(3, 9, 2012), new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "mustermann@web.de", "03783476367", "Interessent"));
@@ -56,12 +62,12 @@ public class Test {
         ////        m.add(new Mieter (90003, "Harry", "Potter", "12.10.1997", -100, "potter@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "03089653", "Mieter"));
         //
         //       //Wohnungen
-        m.add(new Wohnung(301, 4, 80.60, 1100.00, 1, true, true, "Park", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), new Mitarbeiter(6, "Menz", "Max")));
-        m.add(new Wohnung(302, 6, 80.60, 1100.00, 4, true, true, "Park", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), new Mitarbeiter(2, "Tsakonas", "Felicia")));
-        m.add(new Wohnung(303, 6, 80.60, 1100.00, 3, true, true, "Spree", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), new Mitarbeiter(6, "Menz", "Max")));
-        m.add(new Wohnung(304, 3, 80.60, 1100.00, 1, true, true, "Schienen", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), new Mitarbeiter(2, "Tsakonas", "Felicia")));
+        m.add(new Wohnung(301, 4, 80.60, 1100.00, 1, true, true, "Park", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), mMenz));
+        m.add(new Wohnung(302, 6, 80.60, 1100.00, 4, true, true, "Park", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), mTsakonas));
+        m.add(new Wohnung(303, 6, 80.60, 1100.00, 3, true, true, "Spree", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), mMenz));
+        m.add(new Wohnung(304, 3, 80.60, 1100.00, 1, true, true, "Schienen", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), mTsakonas));
         //
-        //      //
+        //      
         //      m.add(new Handwerkerauftrag("H-404", 303, 1, "Kaputte Wand", "in Bearbeitung", new Datum (3, 12, 2016), new Datum (0,2,2002)));
         //      m.add(new Handwerkerauftrag("H-40003", 302, 1, "Kaputte Tür", "beendet", new Datum (1, 12, 2014), new Datum (0,12,2003)));
         //      m.add(new Handwerkerauftrag("H-40002", 303, 2, "Kaputte Badewanne", "beendet", new Datum (1, 11, 2013), new Datum (0,2,2007)));
