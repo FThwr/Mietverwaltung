3b1a7df7b051971d0badba9a789b38a4df0671c4
AG1601 <alicia.guhl@arcor.de>
2017-08-30 14:26:45 +0200

    import java.io.File hinzugefügt

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -8,6 +9,7 @@ import hilfsklassen.Datum;
 import menu.MenuManager;
 import objekte.Handwerkerauftrag;
 import objekte.Interessent;
+import objekte.Mieter;
 import objekte.Mitarbeiter;
 import objekte.Wohnung;
 
@@ -21,7 +23,7 @@ public class Test {
 
         // MenuManager m = new MenuManager();
 
-    	//new File("..\\menumanager.dat").delete();
+//    	new File("..\\menumanager.dat").delete();
 
         MenuManager m;
         try {
@@ -56,10 +58,10 @@ public class Test {
         //
         //
         //      //Mieter
-        //      m.add(new Mieter (90001, "Mustermann", "Max", new Datum(3, 9, 2012), -100, "mustermann@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "03783476367", "Mieter" ));
-        //      m.add(new Mieter (90002, "Musterfrau", "Jana", new Datum(1, 9, 2014), -100, "musterfrau@web.de", new Adresse("Kleeweg", 23, 13357, "Berlin, Wedding"), "03783476367", "Mieter" ));
-        //      //      m.add(new Mieter (90002, "Jana", "Musterfrau","4.6.2002", 100, "musterfrau@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"),"898389439", "Mieter" ));
-        ////        m.add(new Mieter (90003, "Harry", "Potter", "12.10.1997", -100, "potter@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "03089653", "Mieter"));
+              m.add(new Mieter (90001, "Mustermann", "Max", new Datum(3, 9, 2012), -100, "mustermann@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "03783476367", "Mieter" ));
+              m.add(new Mieter (90002, "Musterfrau", "Jana", new Datum(1, 9, 2014), -100, "musterfrau@web.de", new Adresse("Kleeweg", 23, 13357, "Berlin, Wedding"), "03783476367", "Mieter" ));
+//              m.add(new Mieter (90002, "Jana", "Musterfrau","4.6.2002", 100, "musterfrau@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"),"898389439", "Mieter" ));
+//              m.add(new Mieter (90003, "Harry", "Potter", "12.10.1997", -100, "potter@web.de", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "03089653", "Mieter"));
         //
         //       //Wohnungen
         m.add(new Wohnung(301, 4, 80.60, 1100.00, 1, true, true, "Park", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), mMenz));
@@ -68,9 +70,9 @@ public class Test {
         m.add(new Wohnung(304, 3, 80.60, 1100.00, 1, true, true, "Schienen", new Adresse("Kleeweg", 22, 13357, "Berlin, Wedding"), "frei", new Datum(22, 1, 2017), 3, "Rohre neu verlegt", new Handwerkerauftrag(""), mTsakonas));
         //
         //      
-        //      m.add(new Handwerkerauftrag("H-404", 303, 1, "Kaputte Wand", "in Bearbeitung", new Datum (3, 12, 2016), new Datum (0,2,2002)));
-        //      m.add(new Handwerkerauftrag("H-40003", 302, 1, "Kaputte Tür", "beendet", new Datum (1, 12, 2014), new Datum (0,12,2003)));
-        //      m.add(new Handwerkerauftrag("H-40002", 303, 2, "Kaputte Badewanne", "beendet", new Datum (1, 11, 2013), new Datum (0,2,2007)));
+//              m.add(new Handwerkerauftrag("H-404", 303, 1, "Kaputte Wand", "in Bearbeitung", new Datum (3, 12, 2016), new Datum (0,2,2002)));
+//              m.add(new Handwerkerauftrag("H-40003", 302, 1, "Kaputte Tür", "beendet", new Datum (1, 12, 2014), new Datum (0,12,2003)));
+//              m.add(new Handwerkerauftrag("H-40002", 303, 2, "Kaputte Badewanne", "beendet", new Datum (1, 11, 2013), new Datum (0,2,2007)));
         //
         //
         m.start();
